fa5c3fdb5bae658127ea9e23ffc2261846d6fcfc
Luiz Vitor <luizpierinovitor@gmail.com>
2026-04-23 20:45:11 -0300

    Feat: Refatorando código

--- 6 Condicões Aninhadas/Ex9.cs
+++ 6 Condicões Aninhadas/Ex9.cs
@@ -2,80 +2,53 @@
 
 /*
 Jogo pedra papel e tesoura
- */
+*/
 
 using System;
 using System.Linq;
-using System.Globalization;
 
-class Treino
+namespace Fundamentos
 {
-    static void Main()
+    public class Program
     {
-        
-        while (true)
-        {
-            string escolha_jogador;
+        private static Random _escolhaComputador = new Random();
+        private static string[] _opcoes = new string[] { "pedra", "papel", "tesoura" };
 
-            Console.WriteLine("-=-=-= Jogo pedra, papel e tesoura -=-=-=\n");
+        private static void Main(string[] args)
+            => ExibirDados();
+
+
+        private static string RetornarEscolhaComputador()
+            =>_opcoes[_escolhaComputador.Next(_opcoes.Length)]; // _opcoes[índice] 
+
+        private static string RetornarEscolhaUsuario()
+        {
             while (true)
             {
-                
-                Console.Write("\n>Digite: Pedra, Papel ou Tesoura: ");
-                escolha_jogador = Console.ReadLine().Trim().ToLower();
-                if (!string.IsNullOrWhiteSpace(escolha_jogador) && !escolha_jogador.Any(char.IsDigit) && 
-                    (escolha_jogador == "pedra" || escolha_jogador == "tesoura" || escolha_jogador == "papel"))
-                {
-                    break;
-                }
-                else
+                Console.Write("Escolha entre: pedra, papel ou tesoura - ");
+                string entrada = Console.ReadLine().Trim().ToLower();
+                if (string.IsNullOrEmpty(entrada) || !_opcoes.Contains(entrada))
                 {
                     Console.Clear();
-                    Console.WriteLine(">Entrada inválida! Esperava um nome em formato 'string' como; 'pedra, papel ou tesoura'!\n");
-                }
-            }
-        
-            string[] opcoes = { "pedra", "papel", "tesoura" };
-            Random sorteio = new Random();
-
-
-            Console.Clear();
-
-            // Sorteia um índice aleatório dentro do intervalo do array
-            string escolha_maquina = opcoes[sorteio.Next(0, opcoes.Length)];// Obtém o nome correspondente ao índice sorteado
-
-            Console.Write($"\n>Máquina escolheu: {CultureInfo.CurrentCulture.TextInfo.ToTitleCase(escolha_maquina)}\n" +
-                $">Usuário escolheu: {CultureInfo.CurrentCulture.TextInfo.ToTitleCase(escolha_jogador)}\n");
-
-            if (escolha_jogador == escolha_maquina)
-            {
-                Console.WriteLine($">Mesma esolha!\n>Tente novamente!\n");
-                
-            }
-            else if ((escolha_jogador == "pedra" && escolha_maquina == "tesoura") || (escolha_jogador == "papel" && escolha_maquina == "pedra") || (escolha_jogador == "tesoura" && escolha_maquina == "papel"))
-            {
-                Console.WriteLine("\n>Parabéns! Você ganhou!\n");
-            }
-            else
-            {
-                Console.WriteLine("\n>Que pena! A máquina ganhou!\n");
-            }
-
-            Console.Write(">Deseja continuar? [Sim][Não]\n");
-            string op = Console.ReadLine().Trim().ToLower();
-            if (!string.IsNullOrWhiteSpace(op))
-            {
-                if (op == "sim")
-                {
-                    Console.Clear();
+                    Console.WriteLine("Valor inválido. Tente novamente digitando apenas: pedra, papel ou tesoura.");
                     continue;
                 }
-                else if (op == "não")
-                {
-                    Console.WriteLine("\nSaindo..!\n");
-                    break;
-                }
+                return entrada;
             }
         }
+
+        private static string RetornarVencedorJogo(string escolhaComputador, string escolhaUsuario)
+        {
+            Console.Clear();
+            if (escolhaComputador == escolhaUsuario)
+                return $"Empate! Ambos escolheram {escolhaComputador}.";
+            else if ((escolhaComputador == "papel" && escolhaUsuario == "pedra") || (escolhaComputador == "pedra" && escolhaUsuario == "tesoura") || (escolhaComputador == "tesoura" && escolhaUsuario == "papel"))
+                return $"Computador: {escolhaComputador}\nUsuário: {escolhaUsuario}\n\nComputador venceu!";
+            else 
+                return $"Computador: {escolhaComputador}\nUsuário: {escolhaUsuario}\n\nUsuário venceu!";
+        }
+
+        private static void ExibirDados()
+            =>Console.WriteLine(RetornarVencedorJogo(RetornarEscolhaComputador(),RetornarEscolhaUsuario()));
     }
 }
