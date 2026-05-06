259fcff3ea935e50f2d5175af49b8ad7ee9814ae
Luiz Vitor <luizpierinovitor@gmail.com>
2026-05-05 21:33:51 -0300

    Feat: Refatorando código

--- 7 While/Ex2.cs
+++ 7 While/Ex2.cs
@@ -1,147 +1,142 @@
 ﻿
 
+
 /*
-Crie um programa que leia dois valores e mostre um menu na tela:
-[ 1 ] somar
-[ 2 ] multiplicar
-[ 3 ] maior
-[ 4 ] novos números
-[ 5 ] sair do programa
-Seu programa deverá realizar a operação solicitada em cada caso.
- */
+        Crie um programa que leia dois _valores e mostre um menu na tela:
+        [ 1 ] somar
+        [ 2 ] multiplicar
+        [ 3 ] maior
+        [ 4 ] novos números
+        [ 5 ] sair do programa
+        Seu programa deverá realizar a operação solicitada em cada caso.
+*/
+
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
-
-class Treino
+namespace Fundamentos
 {
-    static void Main()
+    public class Program
     {
-        var (n1, n2) = leitura();
-        List<double> novos_numeros = new List<double> { n1, n2 };
+        private static List<double> _listaValores = new List<double>();
 
-        double novos;
-        int opcoes;
-
-        Console.Clear();
-        while (true)
+        private static void Main(string[] args)
+            => ExibirDados();
+        
+        private static void PopularDoisValoresArray()
         {
-            
-            Console.WriteLine("Menu -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n" +
-                    ">Escolha uma das opções abaixo:\n" +
-                    "[ 1 ] somar\n" +
-                    "[ 2 ] multiplicar\n" +
-                    "[ 3 ] maior\n" +
-                    "[ 4 ] novos números\n" +
-                    "[ 5 ] sair do programa\n");
-
-            
-            string op = Console.ReadLine();
-
-            if (int.TryParse(op, out opcoes) && opcoes > 0 && opcoes <= 5)
+            for(int i = 0; i < 2; i++)
             {
-                switch (opcoes)
+                Console.Clear();
+                while (true)
                 {
-                    case 1:
+                    Console.Write($"Entre com o {i+1}ª valor: ");
+                    string entrada = Console.ReadLine().Trim();
+                    if(!double.TryParse(entrada, out double valor) || valor < 0)
+                    {
                         Console.Clear();
-                        Console.WriteLine($">Soma de {n1} + {n2} = {n1 + n2}\n");
-                        break;
-                    case 2:
-                        Console.Clear();
-                        Console.WriteLine($">Multiplicação de {n1} * {n2} = {n1 * n2}\n");
-                        break;
-                    case 3:
-                        Console.Clear();
-                        if (n1 < n2)
-                        {
-                            Console.WriteLine($">Maior número digitado: {n2}\n");
-                        }
-                        else if (n1 > n2)
-                        {
-                            Console.WriteLine($">Maior número digitado: {n1}\n");
-                        }
-                        break;
-                    case 4:
-                        Console.Clear();
-                        Console.WriteLine(">Digite novos números para ser adicionados na sequência: ");
-                        while (true)
-                        {
-                            string n = Console.ReadLine();
-                            if (double.TryParse(n, out novos))
-                            {
-                                novos_numeros.Add(novos);
-                                break;
-                            }
-                            else
-                            {
-                                Console.Clear();
-                                Console.WriteLine(">Número inválido. Esperava um número 'inteiro ou real'!\n");
-                            }
-                        }
-                        // Atualizar n1 e n2 com os dois primeiros números da lista
-                        n1 = novos_numeros[0];
-                        n2 = novos_numeros[1];
-
-                        Console.Clear();
-                        Console.WriteLine("Números adicionados: ");
-                        foreach (var i in novos_numeros)
-                        {
-                            Console.Write($"{i} | ");
-                        }
-                        Console.WriteLine();
-                        break;
-
-                    case 5:
-                        Console.Clear();
-                        Console.WriteLine(">Saindo..\n");
-                        return;
-                    default:
-                        break;
+                        Console.WriteLine("Entrada inválida. Digite um número maior que zero.");
+                        continue;
+                    }
+                    _listaValores.Add(valor);
+                    break;
                 }
             }
-            else
+            Console.Clear();
+        }
+
+        private static void ExibirMenu()
+            =>Console.Write(@"
+Escolha uma das opções abaixo:
+                            [ 1 ] somar
+                            [ 2 ] multiplicar
+                            [ 3 ] maior
+                            [ 4 ] novos números
+                            [ 5 ] sair do programa
+ "); 
+
+        private static void ProcessarOperacoes()
+        {
+            while (true)
             {
+                int numero;
+
+                ExibirMenu();
+                while (true)
+                {
+                    string entrada = Console.ReadLine().Trim();
+                    if(!int.TryParse(entrada, out numero) || numero < 1 || numero > 5)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Entrada inválida. Digite um número de 1 à 5.");
+                        continue;
+                    }
+                    break;
+                }
+
                 Console.Clear();
-                Console.WriteLine(">Número inválido. Esperava um número 'inteiro ou real'!\n");
+            
+                switch (numero)
+                {                
+                    case 1:
+                        Console.WriteLine($"Soma dos valores: {RetornarSomaValores()}");
+                        break;
+                    case 2:
+                        Console.WriteLine($"Multiplicação dos valores: {RetornarMultiplicacaoValores()}");
+                        break;
+                    case 3:
+                        Console.WriteLine($"Maior dos valores: {RetornarMaiorValor()}");
+                        break;
+                    case 4:
+                        Console.WriteLine($"Valores atuais: {string.Join(", ",_listaValores)}.");
+                        AdicionarMaisValores();
+                        break;
+                    case 5:
+                        Console.WriteLine($"Saindo..");
+                        return;
+                }
             }
         }
-    }
 
+        private static double RetornarSomaValores()
+            =>_listaValores.Sum();
 
-    static (double numero1, double numero2) leitura()
-    {
-        double numero1, numero2;
-        Console.WriteLine(">Digite dois números: ");
-        while (true)
+        private static double RetornarMaiorValor()
+            =>_listaValores.Max();
+        
+        private static void AdicionarMaisValores()
         {
-            Console.Write(">1ª: ");
-            string n1 = Console.ReadLine();
-            if (double.TryParse(n1, out numero1) && numero1 > 0)
+            while (true)
             {
+                Console.Write("Entre com mais valores: ");
+                string entrada = Console.ReadLine().Trim();
+                if (!double.TryParse(entrada, out double valor) || valor < 0)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Entrada inválida. Digite um número inteiro ou real maior ou igual a zero.");
+                    continue;
+                }
+                _listaValores.Add(valor);
                 break;
             }
-            else
-            {
-                Console.Clear();
-                Console.WriteLine(">Número inválido. Esperava um número 'inteiro ou real'!\n");
-            }
+            Console.Clear();
+            Console.WriteLine($"Valores atuais: {string.Join(", ", _listaValores)}.");
         }
-        while (true)
+
+                
+        // acc = acumulador(resultado parcial)
+        // x = valor atual do array
+        // -> A cada passo ele faz: acc = acc * x;
+        private static double RetornarMultiplicacaoValores()
+            => _listaValores.Aggregate((acc, x) => acc * x); 
+                
+        
+        private static void ExibirDados()
         {
-            Console.Write(">2ª: ");
-            string n2 = Console.ReadLine();
-            if (double.TryParse(n2, out numero2) && numero2 > 0)
-            {
-                break;
-            }
-            else
-            {
-                Console.Clear();
-                Console.WriteLine(">Número inválido. Esperava um número 'inteiro ou real'!\n");
-            }
+            PopularDoisValoresArray();
+            ProcessarOperacoes();
         }
-        return (numero1, numero2);
     }
 }
-
