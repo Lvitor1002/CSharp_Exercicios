e24ab092bab566612a88dfbff6fcd63b10b3092e
Luiz Vitor <luizpierinovitor@gmail.com>
2026-05-06 20:12:50 -0300

    Feat: Refatorando código

--- 7 While/Ex3.cs
+++ 7 While/Ex3.cs
@@ -1,48 +1,61 @@
 ﻿
-/*
-Faça um programa que leia um número qualquer e mostre o seu fatorial.
 
-Ex: 5! = 5 x 4 x 3 x 2 x 1 = 120
- */
+/*
+    Faça um programa que leia um número qualquer e mostre o seu fatorial.
+
+    Ex: 5! = 5 x 4 x 3 x 2 x 1 = 120
+*/
+
 
 using System;
+using System.Collections.Generic;
 
-
-
-class Treino
+namespace Fundamentos
 {
-    static void Main()
+    public class Program
     {
-        int numero, fatorial = 1;
-        string exibir_x = "";
-        
-        while (true)
+
+        private static void Main(string[] args)
+            => ExibirDados();
+
+        private static int RetornarNumero()
         {
-            Console.Write(">Digite um número qualquer: ");
-            string n = Console.ReadLine();
-            if(int.TryParse(n, out numero) && numero > 0)
+            while (true)
             {
-                break;
-            }
-            else
-            {
-                Console.WriteLine("\n>Entrada inválida! Esperava um número 'inteiro ou real'!");
+                Console.Write("Digite um número: ");
+                string entrada = Console.ReadLine().Trim();
+                if(!int .TryParse(entrada, out int numero) || numero < 0)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Valor inválido. Digite um número inteiro válido e maior que zero.");
+                    continue;
+                }
+                Console.Clear();
+                return numero;
             }
         }
-        
-        for (int i = numero; i > 0; i--) 
+        private static (int, List<int>) RetornarDadosFatorialNumero(int numeroFatorial)
         {
-            fatorial *= i;
-            exibir_x += i;
-            
-            if (i > 1)
+            int resultadoFatorial = 1;
+            var listaSequenciaNumeros = new List<int>();
+
+            for (int i = 1; i <= numeroFatorial; i++)
             {
-                exibir_x += " x ";
+                resultadoFatorial *= i;
+                listaSequenciaNumeros.Add(i);
             }
+
+            return (resultadoFatorial, listaSequenciaNumeros);
         }
 
-        Console.Clear();
-        Console.WriteLine($"\n>Fatorial de {numero}! = {exibir_x} = {fatorial}");
-        
+        private static void ExibirDados()
+        {
+            int numeroFatorial = RetornarNumero();
+            var (resultadoFatorial, listaSequenciaNumeros) = RetornarDadosFatorialNumero(numeroFatorial);
+
+            listaSequenciaNumeros.Reverse();
+            
+            Console.WriteLine($"{numeroFatorial}! {string.Join(" x ", listaSequenciaNumeros)} = {resultadoFatorial}");
+        }
     }
 }
