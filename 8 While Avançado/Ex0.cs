9ec48cb07ec7a499a786bb1d34905e78b468364a
Luiz Vitor <luizpierinovitor@gmail.com>
2026-05-19 20:49:05 -0300

    Feat: Refatorando exercício

--- 8 While Avançado/Ex0.cs
+++ 8 While Avançado/Ex0.cs
@@ -3,46 +3,60 @@
 /*
 Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitado pelo usuário. 
 O programa será interrompido quando a entrada for 'sair'. 
- */
+ */ 
 
 
 using System;
-class Treino
-{
-    static void Main()
-    {
-        int numero;
-        Console.Write("\n>Digite um número para tabuada:\n>Para interromper digite 'sair'\n");
+using System.Linq;
 
-        while (true)
+
+namespace Fundamentos
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+            => ExibirDados();
+
+        private static int? RetornarNumero()
         {
-            
-            string n = Console.ReadLine().Trim().ToLower();
-            if (n == "sair")
+            while (true)
             {
-                Console.Clear();
-                Console.WriteLine("\nSaindo..");
-                break;
-            }
-            
-            if (int.TryParse(n, out numero) && numero > 0)
-            {
-                Console.Clear();
-                Console.WriteLine($"\nTabuada do {numero}:");
-                for (int i = 1; i <= 10; i += 1)
-                {
-                    Console.WriteLine($"\n| {numero,2} x {i,2} = {numero * i,3}  |");
-                }
-                Console.Write("\n>Digite outro número ou 'sair': ");
-                continue;
+                Console.Write("Entre com um valor para a tabuada:\nPara encerrar digite 'sair'.\n- ");
+                string entrada = Console.ReadLine().Trim().ToLower();
+
+                if(entrada == "sair")    
+                    return null;
                 
-            }
-            else
-            {
+                if (!int .TryParse(entrada, out int numero) || numero < 0)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Valor inválido. Digite um número inteiro válido e maior ou igual à zero.");
+                    continue;
+                }
                 Console.Clear();
-                Console.WriteLine("\n>Entrada inválida! Esperava um número 'inteiro' ou 'sair'\n");
+                return numero;
             }
         }
+
+        private static void ExibirTabuadaNumero()
+        {
+            while (true)
+            {
+                int? numero = RetornarNumero();
+
+                if (!numero.HasValue)
+                    break;
+
+                Console.WriteLine($"Tabuada do {numero}:");
+                for(int i = 0; i <= 10; i++)
+                    Console.WriteLine($"{numero} x {i} = {numero * i}");
+                Console.WriteLine();
+            }
+            Console.Clear();
+            Console.WriteLine("Encerrando o programa...");
+        }
+
+        private static void ExibirDados()
+            => ExibirTabuadaNumero();
     }
 }
-
