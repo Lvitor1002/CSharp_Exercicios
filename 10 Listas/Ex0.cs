4602cfc78d72e8e2da57726f58a954abfd4da356
Luiz Vitor <luizpierinovitor@gmail.com>
2026-06-23 20:18:07 -0300

    Codigo novo

--- 10 Listas/Ex0.cs
+++ 10 Listas/Ex0.cs
@@ -1,76 +1,56 @@
 ﻿
 
 /*
-Faça um programa que leia 10 valores numéricos e guarde-os em uma lista. 
-No final, mostre a lista ordenada e qual foi o maior e o menor valor digitado e as suas respectivas POSIÇÕES na lista. 
+    Faça um programa que leia 10 valores numéricos e guarde-os em uma lista. 
+    No final, mostre a lista ordenada e qual foi o maior e o menor valor digitado e as suas respectivas POSIÇÕES na lista. 
 */
+
 
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
-class Treino
+
+namespace Fundamentos
 {
-    static void Main()
+    public static class Program
     {
-        var (valores, menor, maior, pos_menor, pos_maior) = Leitura();
-        Exibir(valores, menor, maior, pos_menor, pos_maior);
-    }
+        private static void Main(string[] args)
+            => ExibirDados();
 
 
-    static void Exibir(List<int> valores, int menor, int maior, int pos_menor, int pos_maior)
-    {
-        Console.Clear();
-        Console.WriteLine(">Todos os valores: \n");
-        for(int i = 0; i < valores.Count; i++)
+        private static List<int> RetornarListaDezValores()
         {
-            Console.WriteLine($"{i+1}ª | {valores[i]}");
-        }
-        Console.Write($"\n>Maior valor {maior} na posição {pos_maior}ª\n\n" +
-                      $">Menor valor {menor} na posição {pos_menor}ª\n\n" +
-                      $">Valores Ordenados: {string.Join(", ", valores.OrderBy(x => x))}.\n\n");
+            var listaNumeros = new List<int>();
 
-    }
-
-    
-    static (List<int> valores, int menor, int maior, int pos_menor, int pos_maior) Leitura()
-    {
-        var valores = new List<int>();
-        int valor, pos_menor = 0, pos_maior;
-
-        int maior = int.MinValue;
-        int menor = int.MaxValue;
-
-        Console.WriteLine(">Digite abaixo 10 valores numéricos: ");
-        for (int i = 0; i < 10; i++)
-        {
-            while (true)
+            for (int i = 0; i < 10; i++)
             {
-                Console.Write($"{i+1}ª: ");
-                string v = Console.ReadLine();
-                if(int.TryParse(v, out valor) && valor >= 0)
+                while (true)
                 {
-                    if(valor > maior)
+                    Console.Write($"Digite o {i + 1}º valor: ");
+                    string entrada = Console.ReadLine().Trim();
+
+                    if (!int.TryParse(entrada, out int valor) || valor < 0)
                     {
-                        maior = valor;
-                        
-                    }else if( valor < menor)
-                    {
-                        menor = valor;
+                        Console.Clear();
+                        Console.WriteLine("Valor inválido. Digite um número inteiro positivo.");
+                        continue;
                     }
-                    valores.Add(valor);
+                    listaNumeros.Add(valor);
                     break;
                 }
-                else
-                {
-                    Console.Clear();
-                    Console.WriteLine(">Entrada inválida. Digite um número 'inteiro' positivo!");
-                }
             }
+            Console.Clear();
+            return listaNumeros;
         }
-        pos_maior = valores.IndexOf(maior) + 1;
-        pos_menor = valores.IndexOf(menor) + 1;
 
-        return (valores, menor, maior, pos_menor, pos_maior);
+        private static void ExibirDados()
+        {
+            var listaNumeros = RetornarListaDezValores();
+
+            Console.WriteLine($"Todos os valores adicionados em ordem: {string.Join(",", listaNumeros.OrderBy(x => x).ToList())}\n");
+            Console.WriteLine($"Todos os valores adicionados na ordem orignal: {string.Join(",", listaNumeros)}");
+            Console.WriteLine($"Maior valor: {listaNumeros.Max()} na posição {listaNumeros.IndexOf(listaNumeros.Max()) + 1}ª.\nMenor valor: {listaNumeros.Min()} na posição {listaNumeros.IndexOf(listaNumeros.Min()) + 1}ª");
+        }
     }
 }
